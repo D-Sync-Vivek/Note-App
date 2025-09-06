788ff42ef7f84ab59f193c9abca1fb5681696131
D-Sync-Vivek <vivekpat931@gmail.com>
2025-09-06 13:49:43 +0530

    Added suspense boundary

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Form from '@/components/Form'
 import Note from '@/components/Notes'
+import { Suspense } from "react";
 export default function Home() {
   return (
     <>
       <main>
         <div className='text-center mt-5 mb-5 text-2xl'>Notes App</div>
+        <Suspense fallback={<div>Loading form...</div>}>
         <Form/>
+        </Suspense>
         <Note/>
       </main>
     </>
